42ab3fa0f026f08a649775ff9b4f119ff7e70597
JesusDSaavedra <iam.jesusd88@gmail.com>
2024-06-08 01:16:35 -0500

    Relaciones en base de datos one to many

--- db/models/product.model.js
+++ db/models/product.model.js
@@ -1,4 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const { CATEGORY_TABLE } = require('./category.model');
 
 const PRODUCT_TABLE = 'products';
 
@@ -17,15 +19,38 @@ const ProductSchema = {
     allowNull: false,
     type: DataTypes.INTEGER,
   },
+  description: {
+    allowNull: false,
+    type: DataTypes.TEXT,
+  },
   image: {
     allowNull: false,
     type: DataTypes.STRING,
   },
+  createdAt: {
+    field: 'created_at',
+    allowNull: false,
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  categoryId: {
+    field: 'category_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: CATEGORY_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
 };
 
 class Product extends Model {
-  static associate() {
-    // Models
+  static associate(models) {
+    this.belongsTo(models.Category, {
+      as: 'category',
+    });
   }
   static config(sequelize) {
     return {
