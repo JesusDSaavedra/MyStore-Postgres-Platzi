c4d768f39df7df1faf4154da5442a50074750ff0
JesusDSaavedra <iam.jesusd88@gmail.com>
2024-06-12 01:25:26 -0500

    configuracion de despliegues 2

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV || 'production',
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
-  dbEngine: process.env.DB_ENGINE, // postgres
+  dbUser: process.env.DATABASE_USER,
+  dbPassword: process.env.DATABASE_PASSWORD,
+  dbHost: process.env.DATABASE_HOST,
+  dbName: process.env.DATABASE_NAME,
+  dbPort: process.env.DATABASE_PORT,
+  dbEngine: process.env.DATABASE_ENGINE, // postgres
   dbUrl: process.env.DATABASE_URL,
 };
 
